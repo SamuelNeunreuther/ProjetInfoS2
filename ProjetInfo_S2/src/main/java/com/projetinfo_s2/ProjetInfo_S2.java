7e0118bf80787144a68a40b4f575019e4d804659
SamuelNeunreuther <samuel.neunreuther@gmail.com>
2026-03-30 16:27:18 +0200

    Add files via upload

---
+++ ProjetInfo_S2/src/main/java/com/projetinfo_s2/ProjetInfo_S2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.projetinfo_s2;
+
+/**
+ *
+ * @author Elève
+ */
+public class ProjetInfo_S2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
