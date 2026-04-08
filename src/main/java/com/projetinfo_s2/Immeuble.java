374621f036726d8b3e41c97d06ddcdac9ea2b4e4
SamueNeunreuther <Elve@172.29.64.114>
2026-04-08 10:59:46 +0200

    modification de la classe immeuble - methode addNiveau - attributs niveauList

--- src/main/java/com/projetinfo_s2/Immeuble.java
+++ src/main/java/com/projetinfo_s2/Immeuble.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.projetinfo_s2;
 
-/**
- *
- * @author obrutin01
- */
+import java.util.ArrayList;
+
 public class Immeuble extends Batiment{
+    
+    private ArrayList<Niveau> niveauList = new ArrayList<>();
+    
+    public Immeuble(int id){
+        super(id, "immeuble", 0);
+        //chaque immeuble a comme type "immeuble" et a pour l'instant 0 niveaux
+        this.niveauList = null;
+    }
+    
+    public void addNiveau(Niveau niveau){
+        this.niveauList.add(niveau);
+        super.setNbNiveaux(super.getNbNiveaux() + 1);
+    }
     
 }
