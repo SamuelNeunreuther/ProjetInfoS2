726c75ca81e3567e84dcdf60ee9f935151c408ab
obrutin01 <obrutin01@SEC-C212-F17.csip.in>
2026-04-08 10:34:14 +0200

    Merge origin/main into HEAD

---
+++ src/main/java/com/projetinfo_s2/Sol.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.projetinfo_s2;
+
+/**
+ *
+ * @author obrutin01
+ */
+public class Sol {
+    
+    private Revetement revetement;
+
+    public Revetement getRevetement() {
+        return revetement;
+    }
+
+    public void setRevetement(Revetement revetement) {
+        this.revetement = revetement;
+    }
+    
+    
+}
