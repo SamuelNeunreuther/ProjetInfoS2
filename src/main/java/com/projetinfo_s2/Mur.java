54ffb9e8d1d6fcd4ce742f975e6e8209b6eab3fe
SamueNeunreuther <Elve@172.29.64.114>
2026-04-14 17:50:26 +0200

    Ajout de la methode afficher pour presque toutes les classes elle renvoie un tableau de string avec les infos importantes ajout des classes segment et point

--- src/main/java/com/projetinfo_s2/Mur.java
+++ src/main/java/com/projetinfo_s2/Mur.java
@@ -3,4 +3,52 @@ package com.projetinfo_s2;
 
 public class Mur {
     
+    private int idMur;
+    private Segment position;
+    private Revetement revetement;
+
+    public Mur(int idMur, Segment position, Revetement revetement) {
+        this.idMur = idMur;
+        this.position = position;
+        this.revetement = revetement;
+    }
+    
+    public float surface(Niveau niveau){
+        return position.longueur()*niveau.getHauteur();
+    }
+    
+    public float prix(Niveau niveau){
+        return this.surface(niveau)*this.revetement.getPrixUnitaire();
+    }
+    
+    public String[] afficher(){
+        String[] infos = new String[2];
+        infos[0] = "Mur " +this.idMur;
+        infos[1] = "Revetement du mur : "+this.revetement.getType();
+        return infos;
+    }
+
+    public int getIdMur() {
+        return idMur;
+    }
+
+    public void setIdMur(int idMur) {
+        this.idMur = idMur;
+    }
+
+    public Segment getPosition() {
+        return position;
+    }
+
+    public void setPosition(Segment position) {
+        this.position = position;
+    }
+
+    public Revetement getRevetement() {
+        return revetement;
+    }
+
+    public void setRevetement(Revetement revetement) {
+        this.revetement = revetement;
+    }
 }
